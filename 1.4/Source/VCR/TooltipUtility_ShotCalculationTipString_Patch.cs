53c09a4c1d84564c1a2e3c04126b1eac00cfb11d
Taranchuk <nfhfyxer@gmail.com>
2022-12-22 22:06:30 +0200

    tooltip patch

---
+++ 1.4/Source/VCR/TooltipUtility_ShotCalculationTipString_Patch.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace VCR
+{
+    [HarmonyPatch(typeof(TooltipUtility), "ShotCalculationTipString")]
+    public static class TooltipUtility_ShotCalculationTipString_Patch
+    {
+        public delegate float GetNonMissChance(Verb_MeleeAttack verb_MeleeAttack, LocalTargetInfo target);
+        public static readonly GetNonMissChance getNonMissChance =
+            AccessTools.MethodDelegate<GetNonMissChance>(AccessTools.Method(typeof(Verb_MeleeAttack), "GetNonMissChance"));
+
+        public delegate float GetDodgeChance(Verb_MeleeAttack verb_MeleeAttack, LocalTargetInfo target);
+        public static readonly GetDodgeChance getDodgeChance =
+            AccessTools.MethodDelegate<GetDodgeChance>(AccessTools.Method(typeof(Verb_MeleeAttack), "GetDodgeChance"));
+
+        public static void Postfix(ref string __result, Thing target)
+        {
+            if (Find.Selector.SingleSelectedThing is Pawn pawn && pawn.equipment != null)
+            {
+                var meleeVerb = pawn.equipment.PrimaryEq?.AllVerbs.OfType<Verb_MeleeAttack>().FirstOrDefault();
+                if (meleeVerb != null)
+                {
+                    StringBuilder stringBuilder = new StringBuilder(__result);
+                    var nonMissChance = getNonMissChance(meleeVerb, target);
+                    var dodgeChance = getDodgeChance(meleeVerb, target);
+                    stringBuilder.AppendLine("VCR.MeleeHitChance".Translate(nonMissChance.ToStringPercent()));
+                    stringBuilder.AppendLine("VCR.TargetDodgeChance".Translate(dodgeChance.ToStringPercent()));
+                    __result = stringBuilder.ToString();
+                }
+            }
+        }
+    }
+}
